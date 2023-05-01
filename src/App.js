ca171f47c98fc06f534c560d81543502312f15bd
Jerry Fitzner <jfitzner1@gmail.com>
2023-04-30 21:23:46 -0400

    Fixed Navbar

--- src/App.js
+++ src/App.js
@@ -1,50 +1,59 @@
-import logo from './Jerry.Fitzner2.png';
+// import logo from './Jerry.Fitzner2.png';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Profile from './Components/Profile';
+// import Navbar from './Components/Navbar';
+// import Profile from './Components/Profile';
 import Demo from './Demo';
+import { Container } from '@mui/material';
+import ResponsiveAppBar from './Components/Navbar';
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Demo />
-      <Navbar />
+      <ResponsiveAppBar />
+      <Container sx={{marginY: 5}}>
+        <Demo />
+      {/* <Navbar /> */}
       {/* <Profile /> */}
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Welcome!
+        Welcome!
         </h1>
         <p>
-          My portfolio is currently under construction.
-          <br/>
-          Please feel free to use the links below:  
+        My portfolio is currently under construction.
+        <br/>
+        Please feel free to use the links below:  
         </p>
         <a
-          className="App-link"
-          href="https://blogs.jerryfitzner.dev"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://blogs.jerryfitzner.dev"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          Blog
+        Blog
         </a>
         <a
-          className="App-link"
-          href="https://www.linkedin.com/in/jerryfitzner/"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://www.linkedin.com/in/jerryfitzner/"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          LinkedIn
+        LinkedIn
         </a>
         <a
-          className="App-link"
-          href="https://github.com/jerryfitzner"
-          target="_blank"
-          rel="noopener noreferrer"
+        className="App-link"
+        href="https://github.com/jerryfitzner"
+        target="_blank"
+        rel="noopener noreferrer"
         >
-          GitHub
+        GitHub
         </a>
       </header> */}
+      </Container>
     </div>
   );
 }
--- src/Demo.js
+++ src/Demo.js
@@ -1,10 +1,41 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import { AccessTime } from '@mui/icons-material';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+            backgroundColor: "blue"
+          }
+        },
+        {
+          props: {
+            variant: "body3",
+          }, 
+          style: {
+            fontsize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 export default function Demo() {
   return (
     <div>
-      <Button variant="contained">Hello World</Button>
+      <ThemeProvider theme={theme}>
+        <AccessTime />
+        <Button variant="contained">Hello World</Button>
+      </ThemeProvider>
     </div>
   );
 }
