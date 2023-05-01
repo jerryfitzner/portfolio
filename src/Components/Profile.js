a4f65a48d4316580f69283d61f7ba125c2d7c6cf
Jerry Fitzner <jfitzner1@gmail.com>
2023-04-30 23:05:47 -0400

    Updates

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,11 +1,26 @@
+import { Avatar, Box, Container, Typography } from "@mui/material";
 import React from "react";
-// import logo from '../Jerry.Fitzner2.png';
+import logo from '../Jerry.Fitzner2.png';
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 const Profile = () => {
   return(
-    <>
-      {/* <Avatar alt="Remy Sharp" src="../Jerry.Fitzner2.png" /> */}
-    </>
+    <Container>
+      <Box >
+        <Avatar 
+          alt="Jerry Fitzner" 
+          src={logo}
+          sx={{
+            width: 400,
+            height: 400,
+            justifyContent: 'Center',
+          }}
+        />
+        <Typography>
+          I am a Software Developer
+        </Typography>
+      </Box>    
+    </Container>
   )
 }
 
