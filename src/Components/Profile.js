b1ab467ed6d09700acb26cc37790ca27a72cd034
Jerry Fitzner <jfitzner1@gmail.com>
2023-05-01 14:46:30 -0400

    Profile Container

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -6,19 +6,39 @@ import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 const Profile = () => {
   return(
     <Container>
-      <Box >
+      <Box 
+        sx={{
+          // display: 'flex'
+          // display: {
+          //   xs: 'block, flex',
+          //   md: 'inline, flex',
+          // },
+          // alignItems: 'center',
+        }}
+      >
+        <Grid2 container spacing={2}>
+          <Grid2 xs={6} md={4}>
         <Avatar 
           alt="Jerry Fitzner" 
           src={logo}
           sx={{
+            // md: {6},
             width: 400,
             height: 400,
-            justifyContent: 'Center',
+            // justifyContent: 'Center',
           }}
         />
-        <Typography>
+        </Grid2>
+        <Grid2 xs={6} md={8}>
+        <Typography
+          sx={{
+            // md: {6},
+          }}
+        >
           I am a Software Developer
         </Typography>
+        </Grid2>
+        </Grid2>
       </Box>    
     </Container>
   )
