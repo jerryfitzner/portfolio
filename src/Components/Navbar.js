9d6c4dd3775bef724306a85f144692db0000ee95
Jerry Fitzner <jfitzner1@gmail.com>
2023-05-08 22:24:46 -0400

    Updated navmenu links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,21 +12,25 @@ import Button from '@mui/material/Button';
 // import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Logo from '../Logo.png';
-// import { Linkedin, GitHub, Book } from '@mui/icons-material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import BookIcon from '@mui/icons-material/Book';
+import { redirect } from 'react-router-dom';
 // import { Grid } from '@mui/material';
 
-// const links = [{
-//     title: "LinkedIn",
-//     icon: <Linkedin />
-//   },{
-//     title: "GitHub",
-//     icon: <GitHub />
-//   },{
-//     title: "Blog",
-//     icon: <Book />
-//   }];
-// const pages = ['Resume', 'Contact', 'Projects', 'Me'];
-// const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const links = [{
+    title: "LinkedIn",
+    icon: <LinkedInIcon />,
+    url: 'https://www.linkedin.com/in/jerryfitzner/'
+  },{
+    title: "GitHub",
+    icon: <GitHubIcon/>,
+    url: 'https://github.com/jerryfitzner'
+  },{
+    title: "Blog",
+    icon: <BookIcon />,
+    url: 'https://blogs.jerryfitzner.dev'
+  }];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -47,6 +51,10 @@ function ResponsiveAppBar() {
   //   setAnchorElUser(null);
   // };
 
+  const redirectUrl = (url) => {
+    return redirect('www.google.com'); 
+  };
+
   return (
     <AppBar position="static"
      sx={{
@@ -65,6 +73,7 @@ function ResponsiveAppBar() {
             //   display: { xs: 'none', md: 'flex' },
             // }}
           />
+          
 
           
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' }, flexDirection: 'row-reverse' }}>
@@ -97,56 +106,26 @@ function ResponsiveAppBar() {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {/* {links.map((link) => ( */}
-                // <MenuItem key={link.title} onClick={handleCloseNavMenu}>
-                  {/* {link.icon} */}
-                  {/* <Typography textAlign="center">{link.title}</Typography> */}
-                {/* </MenuItem> */}
-              // ))}
+              {links.map((link) => ( 
+                <MenuItem key={link.title} onClick={handleCloseNavMenu}>
+                  {link.icon} 
+                  <Typography textAlign="center">{link.title}</Typography>
+                </MenuItem>
+              ))} 
             </Menu>
           </Box>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, flexDirection: 'row-reverse', paddingRight: 4,  }}>
-            {/* {links.map((link) => (
+            {links.map((link) => (
               <Button
-                key={link}
-                onClick={handleCloseNavMenu}
+                key={link.title}
+                onClick={redirectUrl(link.url)}
                 sx={{ my: 2, color: '#229EC3', display: 'block' }}
               >
-                {link.title}
+                {link.icon}{link.title}
               </Button>
-            ))} */}
+            ))}
           </Box>
-
-          {/* <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
         </Toolbar>
       </Container>
     </AppBar>
